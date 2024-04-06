8baf6aae3053ba297b1c4bab9663d49096527169
Semyon Klimov <kemenchik.email>
2024-04-06 20:07:36 +0300

    Add hello message

--- src/main/java/com/example/hello/application/HelloController.java
+++ src/main/java/com/example/hello/application/HelloController.java
@@ -1,13 +1,19 @@
 package com.example.hello.application;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Data
 @RestController
 public class HelloController {
 
+  @Value("${app.message}")
+  private String message;
+
   @GetMapping("/question")
   public String hello() {
-    return "Когда приедете делать свечки?";
+    return message;
   }
 }
